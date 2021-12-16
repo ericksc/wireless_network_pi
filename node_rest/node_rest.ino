060e26758e1db638466fea1d2609630b6e03a6cd
Erick Salas Chaverri <ecsa@gft.com>
2021-12-16 17:00:57 -0600

    Create node_rest.ino
    initial version

---
+++ node_rest/node_rest.ino
@@ -0,0 +1,99 @@
+/*
+ *  Simple hello world Json REST response
+  *  by Mischianti Renzo <https://www.mischianti.org>
+ *
+ *  https://www.mischianti.org/
+ *
+ */
+ 
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+ 
+const char* ssid = "Familia Zalas Rodrigues";
+const char* password = "83115612";
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+int ledPin = 13;      // select the pin for the LED
+int sensorValue = 0;  // variable to store the value coming from the sensor
+String value = "";
+
+ESP8266WebServer server(80);
+ 
+// Serving Hello world
+void getHelloWord() {
+    sensorValue = analogRead(sensorPin);
+    value = String (sensorValue);
+    server.send(200, 
+        "text/json", 
+        "{\"name\": \"world:"  + value +   "\"}"
+    );
+}
+ 
+// Define routing
+void restServerRouting() {
+    server.on("/", HTTP_GET, []() {
+        server.send(200, F("text/html"),
+            F("Welcome to the REST Web Server"));
+    });
+    server.on(F("/helloWorld"), HTTP_GET, getHelloWord);
+}
+ 
+// Manage not found URL
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
+ 
+void setup(void) {
+
+  pinMode(ledPin, OUTPUT);
+  
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+ 
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+ 
+  // Activate mDNS this is used to be able to connect to the server
+  // with local DNS hostmane esp8266.local
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+ 
+  // Set server routing
+  restServerRouting();
+  // Set not found response
+  server.onNotFound(handleNotFound);
+  // Start server
+  server.begin();
+  Serial.println("HTTP server started");
+}
+ 
+void loop(void) {
+  server.handleClient();
+  
+
+}
